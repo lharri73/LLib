bbd2a2f7b0c299082d4926bb92faca4278a9c62b
Landon Harris <lharri73@vols.utk.edu>
2021-12-18 18:32:01 -0500

    working version of new calculator

--- src/calc/main.cpp
+++ src/calc/main.cpp
@@ -9,23 +9,18 @@
 #include <utility>
 #include <tgmath.h>
 
-class Val;
+
 class Val{
     public:
         Val(const std::string &input);
-        Val(double im);
-        ~Val();
-
-        bool immediate;
-        double ival;
-        Val* left;
-        Val* right;
-        char op;
-
-        std::string copy;
 
         double evaluate();
+
+    private:
+        std::vector<double> values;
+        std::vector<char> operators;
 };
+
 
 int main(int argc, char **argv){
     std::string input_str;
@@ -88,7 +83,7 @@ int find_close_paren(const std::string &input){
 
 }
 
-double get_inside_parens(std::string &input, size_t i){
+double get_inside_parens(const std::string &input, size_t i){
     char tmp[input.size() - i + 1];
     std::strncpy(tmp, input.c_str() +i, input.size() - i+1);
     std::string tmp_str(tmp);
@@ -100,155 +95,102 @@ double get_inside_parens(std::string &input, size_t i){
     }
     
     memset(tmp, '\0', input.size() - i + 1);
-    strncpy(tmp, input.c_str()+1, close_paren-1);
+    strncpy(tmp, tmp_str.c_str()+1, close_paren-1);
     tmp_str = tmp;
     Val left(tmp_str);
 
     return left.evaluate();
 }
 
-Val::Val(const std::string &input_orig){
-    copy = input_orig.c_str();
-    std::string input = input_orig;
+double parse_val(std::stringstream &buf){
+    double read;
+    buf >> read;
+    buf.str("");
+    buf.clear();
+    return read;
+}
 
-    while(true){
-        if((input[0] == '[' || input[0] == '(') && 
-                find_close_paren(input) == input.size()-1){
-            // if we have `(expr)` or `[expr]`, then remove the parens
-            printf("started and ended with parens: %s\n", input.c_str());
-            char tmp[input.size()];
-            strncpy(tmp, input.c_str()+1, input.size()-2);
-            tmp[input.size()-2] = '\0';
-            input = tmp;
-        }else{
-            break;
-        }
-    }
-
-    left = nullptr;
-    right = nullptr;
-    op = 0;
-
-    std::stringstream strBuf;
+Val::Val(const std::string &input){
+    std::stringstream buf;
+    buf.str("");
     size_t num_read = 0;
-    strBuf.str();
-
-    for(size_t i = 0; i < input.size(); i++){
-        if(input[i] == ' ' || input[i] == '\n') continue;        
+    for(size_t i=0; i < input.size(); i++){
+        if(input[i] == ' ') continue;
         if(is_operator(input[i])){
-            if(op != -1 && op != 0){
-                fprintf(stderr, "two consecutive operators not allowed\n");
-                exit(1);
+            operators.push_back(input[i]);
+            if(num_read != 0){
+                values.push_back(parse_val(buf));
+                num_read = 0;
             }
-            if(num_read == 0){
-                fprintf(stderr, "operator '%c' expects left operand.\n", input[i]);
-                exit(1);
-            }
-            op = input[i];
-            immediate = false;
             continue;
         }
-        if(input[i] == ']' || input[i] == ')'){
-            fprintf(stderr, "Unmatched '%c'\n", input[i]);
-            exit(1);
-        }
-        if(op != 0 && op != -1){
-            char tmp[input.size() - i + 1];
-            memcpy(tmp, input.c_str() + i, input.size() -i+1);
-            std::string tmp_str(tmp);
-            right = new Val(tmp_str);
-            double tmp_ival;
-            strBuf >> tmp_ival;
-            left = new Val(tmp_ival);
-            break;
-        }
         if(input[i] == '(' || input[i] == '['){
-            double cur_ival = get_inside_parens(input, i);
-
-            size_t j;
-            for(j=close_paren; j < tmp_str.size(); j++){
-                if(tmp_str[j] != ' ') break;
+            double next = get_inside_parens(input, i);
+            values.push_back(next);
+            std::string tmp = input.c_str() + i;
+            i += find_close_paren(input.c_str()+i)+1;
+            if(num_read != 0){
+                values.push_back(parse_val(buf));
+                num_read = 0;
             }
-            if(is_operator(tmp_str[j]))
-                op = tmp_str[j++];
-            else if(tmp_str[j] == '[' || tmp_str[j] == '('){
-                double next_ival = get_inside_parens(input, i);
-
-                std::stringstream vals;
-                vals.str();
-                vals << cur_ival << '*' << next_ival;
-                left = new Val(vals.str());
-
-            }
-
-            memset(tmp, '\0', input.size() - i + 1);
-            strncpy(tmp, tmp_str.c_str()+j, tmp_str.size() - j+1);
-            tmp_str = tmp;
-            right = new Val(tmp_str);
-            break;
+            continue;
+        }else{
+            buf << input[i];
+            num_read++;
         }
-        if(op != 0 && left != nullptr){
-            char tmp[input.size() - i + 1];
-            std::strncpy(tmp, input.c_str() +i, input.size() - i+1);
-            std::string tmp_str(tmp);
-
-            if(left != nullptr){
-                right = new Val(tmp_str);
-                break;
-            }
-        }
-        strBuf << input[i];
-        num_read++;
     }
-    if(op == 0){
-        immediate = true;
-        strBuf >> ival;
+
+    if(num_read != 0){
+        double prev_read;
+        buf >> prev_read;
+        num_read = 0;
+        values.push_back(prev_read);
     }
-}
 
-Val::~Val(){
-    if(left != nullptr)
-        delete left;
-    if(right != nullptr)
-        delete right;
-}
-
-Val::Val(double im){
-    copy = "immediate";
-    immediate = true;
-    ival = im;
 }
 
 double Val::evaluate(){
-    double retVal;
-    if(immediate){
-        retVal = ival;
-    }else{
-        switch(op){
-            case -1:
-                retVal = left != nullptr ? left->evaluate() : right->evaluate();
-                break;
-            case '+':
-                retVal = left->evaluate() + right->evaluate();
-                break;
-            case '-': 
-                retVal = left->evaluate() - right->evaluate();
-                break;
-            case '*': 
-            //case -1:
-                retVal = left->evaluate() * right->evaluate();
-                break;
-            case '/': 
-                retVal = left->evaluate() / right->evaluate();
-                break;
-            case '^': 
-                retVal = pow(left->evaluate(), right->evaluate());
-                break;
-            default:
-                fprintf(stderr, "operation %c(%x) not permitted\n", op, op);
-                exit(-1);
+    if(values.size() != operators.size()+1){
+        fprintf(stderr, "operators require left and right operands\n");
+        exit(1);
+    }
+    for(size_t i = 0; i < operators.size(); i++){
+        if(operators[i] == '^'){
+            values[i] = pow(values[i], values[i+1]);
+            values.erase(values.begin()+i+1);
+            operators.erase(operators.begin()+i);
+            i--;
         }
     }
-    printf("retVal: (%s) %f\n", copy.c_str(), retVal);
-    return retVal;
+    for(size_t i = 0; i < operators.size(); i++){
+        if(operators[i] == '*'){
+            values[i] = values[i] * values[i+1];
+            values.erase(values.begin()+i+1);
+            operators.erase(operators.begin()+i);
+            i--;
+            continue;
+        }
+        if(operators[i] == '/'){
+            values[i] = values[i] / values[i+1];
+            values.erase(values.begin()+i+1);
+            operators.erase(operators.begin()+i);
+            i--;
+        }
+    }
+    for(size_t i = 0; i < operators.size(); i++){
+        if(operators[i] == '+'){
+            values[i] = values[i] + values[i+1];
+            values.erase(values.begin()+i+1);
+            operators.erase(operators.begin()+i);
+            i--;
+            continue;
+        }
+        if(operators[i] == '-'){
+            values[i] = values[i] - values[i+1];
+            values.erase(values.begin()+i+1);
+            operators.erase(operators.begin()+i);
+            i--;
+        }
+    }
+    return values[0];
 }
